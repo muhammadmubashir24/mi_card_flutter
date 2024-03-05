add51e46a038f6d4cae9e14f22f0792b7d1170a6
muhammadmubashir24 <muhammad.mubashir2498@gmail.com>
2024-03-05 07:58:55 +0500

    Challenge Complete Task

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                // height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                // child: const Text('Container 1'),
+              ),
+              const SizedBox(width: 30.0),
+              Container(
+                width: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                // color: Colors.blue,
+                // child: const Text('Container 2'),
+              ),
+              const SizedBox(width: 30.0),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                // child: const Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
